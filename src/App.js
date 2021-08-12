0e9505f1d30bc201c2108207f7d646e73cdac30d
NawelTajouri <Lewan.nawel95@gmail.com>
2021-08-12 20:25:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button,Form} from 'react-bootstrap';
+
 import './App.css';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="outer">
+      <div className="container">
+        <div className="HeadForm mb-2">
+          <h5>Sign In</h5>
+        </div>
+        
+        <Form className="MyForm">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label className="formLabel">Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label className="formLabel">Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+
+        </Form>
+        <div className="Mybutton">
+        <Button className="btn mt-3" variant="primary" type="submit">
+            Submit
+          </Button>
+        </div>
+        
+      </div>
+      </div>
+      
+      
     </div>
   );
 }
